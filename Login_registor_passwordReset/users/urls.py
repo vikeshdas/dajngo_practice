15d5b632072149fe6d749894edc9c04717b81e5c
Vikesh <Vikesh.das@subtlelabs.com>
2024-03-17 15:13:34 +0530

    added portfolio and login_signup in django

---
+++ Login_registor_passwordReset/users/urls.py
@@ -0,0 +1,11 @@
+# users/urls.py
+
+from django.urls import path, include
+from users.views import dashboard, register
+
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/", dashboard, name="dashboard"),
+    path("oauth/", include("social_django.urls")),
+    path("register/", register, name="register"),
+]
